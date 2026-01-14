dc3926cc9a01df2cf4c9f4613a9c3d72c2ede630
ivalet-lgtm <lmgeliashvili@gmail.com>
2026-01-15 00:29:49 +0400

    Update telegram_test.py

--- telegram_test.py
+++ telegram_test.py
@@ -1,7 +1,7 @@
 import telebot
 
 # Paste your Token from BotFather here
-BOT_TOKEN = "8504559981:AAFspiT9zI1QrKL-lBibJsTzVndVl6F380Y"
+BOT_TOKEN = "YOUR_TOKEN_HERE"
 # Paste your ID from userinfobot here
 CHAT_ID = "6196771679"
 
@@ -13,4 +13,5 @@ try:
     bot.send_message(CHAT_ID, "Hello! Your Python Bot is now online. 🚀")
     print("Success: Message sent to your Telegram!")
 except Exception as e:
+
     print(f"Error: Could not send message. {e}")
